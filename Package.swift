86947720c79c838db8b5456f7e32d4de6e12d85b
Carlos Monzon <carlosmonzonsalvador@gmail.com>
2023-01-10 17:32:23 +1000

    KMM SPM package release for 0.1.3

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://api.github.com/repos/carlosmonzon/SegmentMultiplatform/releases/assets/81945643.zip"
-let remoteKotlinChecksum = "df2de5eb2611ce107af071524b8b39897811dcbc470a42d830b480aa2a05b951"
+let remoteKotlinUrl = "https://api.github.com/repos/carlosmonzon/SegmentMultiplatform/releases/assets/91125818.zip"
+let remoteKotlinChecksum = "e16bf6ac912a53a4a2582cdb8df7c9070ce3b0589b6213ab322fb3f9be46711e"
 let packageName = "SegmentMultiplatform"
 
 let package = Package(
